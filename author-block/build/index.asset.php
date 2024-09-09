9cac194f3081ee1de084d6e522844e8f0cd06cb0
Shana <shanaallenholder@hotmail.co.uk>
2024-09-09 11:50:38 +0100

    project files initialised

---
+++ author-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '67b3fe03aca4bb20b474');
---
+++ author-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '31b297a74c185e64c942');
